61519fbab56b96d5bc893fa9be5e41de9568942c
Alex3925 <120993369+Alex3925@users.noreply.github.com>
2025-07-18 21:35:46 +0800

    Update checktt.js

--- Script/commands/checktt.js
+++ Script/commands/checktt.js
@@ -1,72 +1,80 @@
 module.exports.config = {
 	name: "checktt",
-	version: "1.0.5",
-	hasPermssion: 0,
-	credits: "Mirai Team",
-	description: "Kiểm tra lượt tương tác trong nhóm",
-	commandCategory: "system",
-	usages: "[all/tag]",
+	version: "1.0.6", // Updated version
+	hasPermssion: 0, // Accessible to all users
+	credits: "Alex Jhon Ponce", // Updated credits
+	description: "Check message interaction counts in the group",
+	commandCategory: "group",
+	usages: "[all/@tag]",
 	cooldowns: 5,
-    envConfig: {
-        "maxColumn": 10
-    }
+	envConfig: {
+		"maxColumn": 10
+	}
 };
 
 module.exports.languages = {
-    "vi": {
-        "all": "%1/ %2 với %3 tin nhắn\n",
-        "mention": "%1 đứng hạng %2 với %3 tin nhắn",
-        "myself": "Bạn đang đứng hạng %1 với %2 tin nhắn"
-    },
-    "en": {
-        "all": "%1/ %2 with %3 messages\n",
-        "mention": "%1 on top %2 with %3 messages",
-        "myself": "You are on top %1 with %2 messages "
-    }
-}
+	"en": {
+		"all": "%1. %2: %3 messages",
+		"mention": "%1 is ranked #%2 with %3 messages",
+		"myself": "You are ranked #%1 with %2 messages",
+		"noData": "No interaction data found for the group."
+	}
+};
 
 module.exports.run = async function ({ args, api, event, Currencies, getText }) {
-    var mention = Object.keys(event.mentions);
-    try {
-        const data = await api.getThreadInfo(event.threadID);
-        if (args[0] == "all") {
-            var number = 1, msg = "", storage = [], exp = [];
-            for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
-            for (const user of storage) {
-                const countMess = await Currencies.getData(user.id);
-                exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp});
-            }
-            exp.sort(function (a, b) { return b.exp - a.exp });
+	const { threadID, messageID, mentions, senderID } = event;
 
-            for (const lastData of exp)  msg += getText("all", number++, lastData.name, lastData.exp);
-            return api.sendMessage(msg, event.threadID);
-        }
-        else if (mention[0]) {
-            var storage = [], exp = [];
-            for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
+	try {
+		const threadInfo = await api.getThreadInfo(threadID);
+		const userInfo = threadInfo.userInfo || [];
 
-            for (const user of storage) {
-                const countMess = await Currencies.getData(user.id);
-                exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
-            }
-            exp.sort(function (a, b) { return b.exp - a.exp });
+		const users = userInfo.map(user => ({
+			id: user.id,
+			name: user.name || "Unknown"
+		}));
 
-            const rank = exp.findIndex(info => parseInt(info.uid) == parseInt(mention[0])) + 1;
-            const infoUser = exp[rank - 1];
-            return api.sendMessage(getText("mention", infoUser.name, rank, infoUser.exp), event.threadID);
-        }
-        else {
-            var storage = [], exp = [];
-            for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
-            for (const user of storage) {
-                const countMess = await Currencies.getData(user.id);
-                exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
-            }
-            exp.sort(function (a, b) { return b.exp - a.exp });
+		const exp = await Promise.all(users.map(async user => {
+			const userData = await Currencies.getData(user.id);
+			return {
+				name: user.name,
+				exp: userData?.exp ?? 0,
+				uid: user.id
+			};
+		}));
+		exp.sort((a, b) => b.exp - a.exp);
 
-            const rank = exp.findIndex(info => parseInt(info.uid) == parseInt(event.senderID)) + 1;
-            const infoUser = exp[rank - 1];
-            return api.sendMessage(getText("myself", rank, infoUser.exp), event.threadID);
-        }
-    } catch (e) { return console.log(e) }
-}
+		if (args[0]?.toLowerCase() === "all") {
+			if (exp.length === 0) return api.sendMessage(getText("noData"), threadID, messageID);
+			const msg = exp.map((user, index) => getText("all", index + 1, user.name, user.exp)).join("\n");
+			return api.sendMessage(
+				`══✦ Group Interaction Rankings ✦══\n${msg}\nPowered by AlexRebornV2`,
+				threadID,
+				messageID
+			);
+		}
+
+		if (Object.keys(mentions).length === 1) {
+			const mentionID = Object.keys(mentions)[0];
+			const rank = exp.findIndex(info => info.uid === mentionID) + 1;
+			if (rank === 0) return api.sendMessage(`No interaction data for ${mentions[mentionID].replace(/@/g, "")}.`, threadID, messageID);
+			const user = exp[rank - 1];
+			return api.sendMessage(
+				`══✦ User Interaction ✦══\n${getText("mention", user.name, rank, user.exp)}\nPowered by AlexRebornV2`,
+				threadID,
+				messageID
+			);
+		}
+
+		const rank = exp.findIndex(info => info.uid === senderID) + 1;
+		if (rank === 0) return api.sendMessage(getText("noData"), threadID, messageID);
+		const user = exp[rank - 1];
+		return api.sendMessage(
+			`══✦ Your Interaction ✦══\n${getText("myself", rank, user.exp)}\nPowered by AlexRebornV2`,
+			threadID,
+			messageID
+		);
+	} catch (e) {
+		console.error("Error in checktt:", e);
+		return api.sendMessage("❌ An error occurred while fetching interaction data.", threadID, messageID);
+	}
+};
